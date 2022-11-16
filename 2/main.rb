d159b6106f11c2d4b1de761434f3f3f202b6f57e
Daniel Sateler <62968964+satelerd@users.noreply.github.com>
2022-11-16 18:41:33 +0000

    describing the problem

---
+++ 2/main.rb
@@ -0,0 +1,19 @@
+# Escribe código en Ruby que use la API pinballmap y:
+
+# Obtenga todas las ubicaciones para las primeras n regiones.
+
+# Usando la información obtenida obtenga las siguientes estadísticas:
+#   Cantidad total de máquinas.
+#   Promedio de máquinas por ubicación.
+#   Ubicación con mayor cantidad de máquinas.
+#   Ubicación con menor cantidad de máquinas.
+
+# Muestre por pantalla las estadísticas con el siguiente formato:
+#   Estadísticas:
+#   **************************************************
+#   Cantidad de máquinas: [número]
+#   Promedio de máquinas: [número]
+#   Ubicación con mayor cantidad de máquinas: [nombre]
+#   Ubicación con menor cantidad de máquinas: [nombre]
+
+# -----------------------------------------------------------------------------
