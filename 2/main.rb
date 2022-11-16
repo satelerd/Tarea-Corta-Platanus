9ed260208597af5cd9a9293b4c1bc596892856d1
Daniel <satelerd@gmail.com>
2022-11-16 19:58:25 -0300

    super clean

--- 2/main.rb
+++ 2/main.rb
@@ -1,32 +1,10 @@
-# Escribe código en Ruby que use la API pinballmap y:
-
-# Obtenga todas las ubicaciones para las primeras n regiones.
-
-# Usando la información obtenida obtenga las siguientes estadísticas:
-#   Cantidad total de máquinas.
-#   Promedio de máquinas por ubicación.
-#   Ubicación con mayor cantidad de máquinas.
-#   Ubicación con menor cantidad de máquinas.
-
-# Muestre por pantalla las estadísticas con el siguiente formato:
-#   Estadísticas:
-#   **************************************************
-#   Cantidad de máquinas: [número]
-#   Promedio de máquinas: [número]
-#   Ubicación con mayor cantidad de máquinas: [nombre]
-#   Ubicación con menor cantidad de máquinas: [nombre]
-
-# Ten en cuenta que la línea de * tiene que tener el ancho del string más grande del reporte.
-
-# -----------------------------------------------------------------------------
-
 require 'httparty'
 require 'json'
 
 
-# Funciones
+# Functions
 # -------------------
-def get_locations(base_url) # Obtener las primeras n regiones
+def get_locations(base_url)
     begin
         print "¿En cuántas regiones quieres buscar? "
         n = Integer(gets.chomp)
@@ -40,9 +18,8 @@ def get_locations(base_url) # Obtener las primeras n regiones
     regions = JSON.parse(response.body)['regions'][0..n-1]
     regions = regions.map { |region| { name: region['name'], id: region['id'] } }
     
-    # Separamos las regiones en ubicaciones
     locations = []
-    regions.each do |region|
+    regions.each do |region| # Get the locations for each region
         region = region[:name]
         response = HTTParty.get(base_url + '/region/' + region + '/locations.json')
         locations += JSON.parse(response.body)['locations'].map { |location| { id: location['id'], name: location['name'], num_machines: location['num_machines'] } }
@@ -52,15 +29,25 @@ def get_locations(base_url) # Obtener las primeras n regiones
 end
 
 
-def get_stats(locations) # Obtener las estadísticas
-    # Calculamos las estadísticas
+def get_stats(locations, display_all_locations)
+    # Calculate the stats
     total_machines = locations.map { |location| location[:num_machines] }.reduce(:+)
     average_machines = total_machines.to_f / locations.length
     max_machines = locations.max_by { |location| location[:num_machines] }
     min_machines = locations.min_by { |location| location[:num_machines] }
+    
+    if display_all_locations
+        max_machines = locations.select { |location| location[:num_machines] == locations.max_by { |location| location[:num_machines] }[:num_machines] }
+        min_machines = locations.select { |location| location[:num_machines] == locations.min_by { |location| location[:num_machines] }[:num_machines] }
+        max_machines = max_machines.map { |location| location[:name] }
+        min_machines = min_machines.map { |location| location[:name] }
+    else
+        max_machines = max_machines[:name]
+        min_machines = min_machines[:name]
+    end
 
-    # Calculamos el ancho de la línea de *
-    lines = ["Estadísticas:", "*", "Cantidad de máquinas: #{total_machines}", "Promedio de máquinas: #{average_machines}", "Ubicación con mayor cantidad de máquinas: #{max_machines[:name]}", "Ubicación con menor cantidad de máquinas: #{min_machines[:name]}"]
+    # Calculate the width of the * line
+    lines = ["Estadísticas:", "*", "Cantidad de máquinas: #{total_machines}", "Promedio de máquinas: #{average_machines}", "Ubicación con mayor cantidad de máquinas: #{max_machines}", "Ubicación con menor cantidad de máquinas: #{min_machines}"]
     max_width = lines.max_by(&:length).length
     lines[1] = "*" * max_width
 
@@ -68,16 +55,13 @@ def get_stats(locations) # Obtener las estadísticas
 end
 
 
-# Variables y llamadas de funciones
+# Main
 # -------------------
-base_url = 'http://pinballmap.com/api/v1'
+if __FILE__ == $0
+    display_all_locations = false  # If true, it will display all the locations with the same number of max and min machines instead of just one
+    base_url = 'http://pinballmap.com/api/v1'
 
-
-locations = get_locations(base_url)
-stats = get_stats(locations)
-puts stats
-
-
-# Dudas
-# -------------------
-# Si hay varias ubicaciones con el mismo mayor o menor número de máquinas, las muestro todas?
+    locations = get_locations(base_url)
+    stats = get_stats(locations, display_all_locations)
+    puts stats
+end
