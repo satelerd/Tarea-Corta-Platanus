ca8f2600bfa4149cde48ddda6dba71178e8f8b85
Daniel Sateler <62968964+satelerd@users.noreply.github.com>
2022-11-16 19:57:47 +0000

    get_locations function

--- 2/main.rb
+++ 2/main.rb
@@ -17,3 +17,36 @@
 #   Ubicación con menor cantidad de máquinas: [nombre]
 
 # -----------------------------------------------------------------------------
+
+require 'httparty'
+require 'json'
+
+# Variables estaticas
+# -------------------
+base_url = 'http://pinballmap.com/api/v1'
+
+# Funciones
+# -------------------
+def get_locations(n, base_url) # Obtener las primeras n regiones
+    response = HTTParty.get(base_url + '/regions')
+    regions = JSON.parse(response.body)['regions'][0..n-1]
+    regions = regions.map { |region| { name: region['name'], id: region['id'] } }
+
+    puts "Hay #{regions.length} regiones"
+    
+    locations = []
+    regions.each do |region|
+        region = region[:name]
+        response = HTTParty.get(base_url + '/region/' + region + '/locations.json')
+
+        locations += JSON.parse(response.body)['locations'].map { |location| { id: location['id'], name: location['name'], num_machines: location['num_machines'] } }
+    end
+
+    puts locations
+    return locations
+end
+
+
+# Llamadas de funciones
+# -------------------
+locations = get_locations(3, base_url)
